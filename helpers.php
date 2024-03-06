2833c7877d9823460e2f1f207b2e8a4cc8b4c016
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-03-06 23:11:32 +0000

    Initial commit and started home view

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+//below is called a doc block
+/**
+ * Get the base path
+ * @params string $path
+ * @return string
+ */
+
+function basePath($path = '') {
+    return __DIR__ . '/' . $path;
+}
+
