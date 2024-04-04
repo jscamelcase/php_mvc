1434a18d91bba7cf66225d5c5368f610b8d7c47a
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-04-04 21:03:04 +0100

    setup login and logout functionality

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -128,7 +128,22 @@ class UserController
 
     ]);
 
-    inspectAndDie(Session::get('user'));
+
+    redirect('/');
+  }
+
+  /**
+   * 
+   * Logout a user and kill session
+   * 
+   * @return void
+   */
+  public function logout()
+  {
+    Session::clearAll();
+
+    $params = session_get_cookie_params();
+    setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
 
     redirect('/');
   }
